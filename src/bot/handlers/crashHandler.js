f5ef42d433621b4f42e8e075657b47a169eb4339
Bardia Shafaee <shafaeebardia@gmail.com>
2023-06-17 13:36:22 -0700

    added crash handler

--- src/bot/handlers/crashHandler.js
+++ src/bot/handlers/crashHandler.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, WebhookClient } = require('discord.js');
+const { EmbedBuilder, WebhookClient, Client } = require('discord.js');
 const { inspect } = require('util');
 const chalk = require('chalk');
 const { log } = require('../../util/helpers/log.js');
@@ -16,30 +16,146 @@ module.exports = client => {
     const webhook = new WebhookClient({
         url: require('../../../config/bot.json').ERROR_LOGGING.URL,
     });
-    const embed = new EmbedBuilder().setTimestamp().setColor(colors.ERROR);
+    let embeds = [new EmbedBuilder().setColor(colors.ERROR)];
 
     // Discord API / Bot client error
     client.on('error', err => {
+        embeds = [new EmbedBuilder().setColor(colors.ERROR)];
         log(
-            chalk.bgYellow.bold(' SERVER '),
+            chalk.bgCyan.bold(' SERVER '),
             true,
             chalk.yellow.bold('... '),
-            chalk.bold('ERROR '),
-            chalk.blue('———[Bot Client/Discord API Error]———')
+            chalk.blue('Bot Client/Discord API Error')
         );
         console.log(err);
 
-        embed
-            .setTitle('Discord Client API Error')
+        embeds[0]
             .setDescription(
                 `\`\`\`${inspect(err, { depth: 0 }).slice(0, 1000)}\`\`\``
-            );
+            )
+            .setTimestamp();
 
-        return webhook.send({ embeds: [embed] });
+        return webhook.send({
+            content: '# Bot Client/Discord API Error',
+            embeds: embeds,
+        });
     });
 
     // NodeJS Error: unhandledRejection
+    process.on('unhandledRejection', (reason, promise) => {
+        embeds = [new EmbedBuilder().setColor(colors.ERROR)];
+        log(
+            chalk.bgCyan.bold(' SERVER '),
+            true,
+            chalk.yellow.bold('... '),
+            chalk.redBright('Unhandled Rejection')
+        );
+        console.log(reason, promise);
+
+        embeds[0]
+            .setTitle('Reason')
+            .setDescription(
+                `\`\`\`${inspect(reason, { depth: 0 }).slice(0, 1000)}\`\`\``
+            );
+        embeds.push(
+            new EmbedBuilder()
+                .setColor(colors.ERROR)
+                .setTitle('Promise')
+                .setDescription(
+                    `\`\`\`${inspect(promise, { depth: 0 }).slice(
+                        0,
+                        1000
+                    )}\`\`\``
+                )
+                .setTimestamp()
+        );
+
+        return webhook.send({
+            content: '# Unhandled Rejection',
+            embeds: embeds,
+        });
+    });
+
     // NodeJS Error: uncaughtException
+    process.on('uncaughtException', (err, origin) => {
+        embeds = [new EmbedBuilder().setColor(colors.ERROR)];
+        log(
+            chalk.bgCyan.bold(' SERVER '),
+            true,
+            chalk.yellow.bold('... '),
+            chalk.redBright('Uncaught Exception')
+        );
+        console.log(err, origin);
+
+        embeds[0]
+            .setTitle('Error')
+            .setDescription(
+                `\`\`\`${inspect(err, { depth: 0 }).slice(0, 1000)}\`\`\``
+            );
+        embeds.push(
+            new EmbedBuilder()
+                .setTitle('Origin')
+                .setDescription(`\`\`\`${inspect(origin, { depth: 0 })}\`\`\``)
+                .setColor(colors.ERROR)
+                .setTimestamp()
+        );
+
+        return webhook.send({
+            content: '# Uncaught Exception',
+            embeds: embeds,
+        });
+    });
+
     // NodeJS Error: uncaughtExceptionMonitor
+    process.on('uncaughtExceptionMonitor', (err, origin) => {
+        embeds = [new EmbedBuilder().setColor(colors.ERROR)];
+        log(
+            chalk.bgCyan.bold(' SERVER '),
+            true,
+            chalk.yellow.bold('... '),
+            chalk.redBright('Uncaught Exception')
+        );
+        console.log(err, origin);
+
+        embeds[0]
+            .setTitle('Error')
+            .setDescription(
+                `\`\`\`${inspect(err, { depth: 0 }).slice(0, 1000)}\`\`\``
+            );
+        embeds.push(
+            new EmbedBuilder()
+                .setTitle('Origin')
+                .setDescription(`\`\`\`${inspect(origin, { depth: 0 })}\`\`\``)
+                .setColor(colors.ERROR)
+                .setTimestamp()
+        );
+
+        return webhook.send({
+            content: '# Uncaught Exception Monitor',
+            embeds: embeds,
+        });
+    });
+
     // NodeJS Error: warning
+    process.on('warning', warn => {
+        embeds = [new EmbedBuilder().setColor(colors.WARNING)];
+        log(
+            chalk.bgCyan.bold(' SERVER '),
+            true,
+            chalk.yellow.bold('... '),
+            chalk.yellow('Warning')
+        );
+        console.log(warn);
+
+        embeds[0]
+            .setDescription(
+                `\`\`\`${inspect(warn, { depth: 0 }).slice(0, 1000)}\`\`\``
+            )
+            .setTimestamp();
+
+        return webhook.send({
+            content: '# Warning',
+            embeds: embeds,
+        });
+    });
 };
