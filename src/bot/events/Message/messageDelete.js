53dd36e3e075374658bf07a3f12ef33b0c1546cb
aName <shafaeebardia@gmail.com>
2023-09-04 18:24:17 -0700

    Add logging (#7)
    * add message deletion logging
    
    * fixed github actions issues
    
    * add message event listeners and logging
    
    * added member logging

---
+++ src/bot/events/Message/messageDelete.js
@@ -0,0 +1,97 @@
+const { EmbedBuilder, Message, Events } = require('discord.js');
+const Settings = require('../../../private/mongodb/guildSettings.js');
+const colors = require('../../../../config/colors.json');
+
+module.exports = {
+    name: Events.MessageDelete,
+    /**
+     *
+     * @param {Message} message
+     */
+    async execute(message) {
+        const guild = message.guild;
+        const settings = await Settings.findOne({ GuildID: guild.id });
+        if (!settings) return;
+        const channelLog = settings.MsgEventChannel;
+        if (!channelLog) return;
+
+        if (message.author == null) {
+            const embed = new EmbedBuilder()
+                .setTitle('Deleted Message')
+                .setColor(colors.EMBED_INVIS_SIDEBAR)
+                .setAuthor({ name: 'Unknown user' })
+                .setDescription(
+                    `${message.content ? message.content : '`None`'}`.slice(
+                        0,
+                        4096
+                    )
+                );
+            const channel = guild.channels.cache.get(`${channelLog}`);
+            return channel
+                ? channel
+                      .send({ embeds: [embed] })
+                      .catch(err => console.error(err))
+                : null;
+        }
+
+        if (message.author.bot) return;
+
+        const Log = new EmbedBuilder()
+            .setColor(colors.EMBED_INVIS_SIDEBAR)
+            .setDescription(
+                `A message sent by <@${message.author.id}> was deleted in <#${message.channel.id}>`
+            )
+            .setTimestamp();
+        const Message = new EmbedBuilder()
+            .setColor(colors.EMBED_INVIS_SIDEBAR)
+            .setTitle('Deleted Message')
+            .setDescription(
+                `${
+                    message.content ? message.content : '`No message content`'
+                }`.slice(0, 4096)
+            );
+        const Attachments = new EmbedBuilder()
+            .setColor(colors.EMBED_INVIS_SIDEBAR)
+            .setTitle('Attachments');
+
+        if (message.attachments.size > 0) {
+            Attachments.addFields({
+                name: `${
+                    message.attachments.size == 1
+                        ? `${message.attachments.size} attachment`
+                        : `${message.attachments.size} attachments`
+                }`,
+                value: `${message.attachments.map(a => a.url).join('\n')}`,
+                inline: false,
+            });
+
+            message.attachments.map(a => {
+                if (
+                    !a.url.endsWith('.png') ||
+                    !a.url.endsWith('.jpg') ||
+                    !a.url.endsWith('.jpeg') ||
+                    !a.url.endsWith('.mov') ||
+                    !a.url.endsWith('.mp4')
+                )
+                    Attachments.setDescription(
+                        '⚠️ Some of these attachments may need to be downloaded to be viewed and may harm your system. **Download these attachments at your own risk**'
+                    );
+            });
+
+            if (!message.content)
+                await guild.channels.cache
+                    .get(`${channelLog}`)
+                    .send({ embeds: [Log, Attachments] })
+                    .catch(e => console.log(e));
+            else
+                await guild.channels.cache
+                    .get(`${channelLog}`)
+                    .send({ embeds: [Log, Message, Attachments] })
+                    .catch(e => console.log(e));
+        } else {
+            await guild.channels.cache
+                .get(`${channelLog}`)
+                .send({ embeds: [Log, Message] });
+        }
+    },
+};
---
+++ src/bot/events/Message/messageUpdate.js
@@ -0,0 +1,67 @@
+const { EmbedBuilder, Message, Events } = require('discord.js');
+const Settings = require('../../../private/mongodb/guildSettings.js');
+const colors = require('../../../../config/colors.json');
+
+module.exports = {
+    name: Events.MessageUpdate,
+    /**
+     *
+     * @param {Message} oldMessage
+     * @param {Message} newMessage
+     */
+    async execute(oldMessage, newMessage) {
+        const guild = oldMessage.guild;
+        const settings = await Settings.findOne({ GuildID: guild.id });
+        if (!settings) return;
+        const channelLog = settings.MsgEventChannel;
+        if (!channelLog) return;
+
+        await oldMessage.fetch(true);
+        await newMessage.fetch(true);
+
+        const maxLength = 4096;
+
+        const Original =
+            oldMessage.content.length > maxLength
+                ? oldMessage.content.slice(0, maxLength - 3) + '...'
+                : oldMessage.content;
+        const Edited =
+            newMessage.content.length > maxLength
+                ? newMessage.content.slice(0, maxLength - 3) + '...'
+                : newMessage.content;
+
+        const OriginalEmbed = new EmbedBuilder()
+            .setColor(colors.EMBED_INVIS_SIDEBAR)
+            .setTitle('Original')
+            .setDescription(`${Original}`);
+        const EditedEmbed = new EmbedBuilder()
+            .setColor(colors.EMBED_INVIS_SIDEBAR)
+            .setTitle('Edited')
+            .setDescription(`${Edited}`)
+            .setTimestamp();
+
+        if (oldMessage.author == null || newMessage.author == null) {
+            const embed = new EmbedBuilder()
+                .setColor(colors.EMBED_INVIS_SIDEBAR)
+                .setTitle('Message edited')
+                .setAuthor({ name: 'Unknown user' });
+
+            const channel = guild.channels.cache.get(`${channelLog}`);
+            return channel
+                ? channel.send({ embeds: [embed, OriginalEmbed, EditedEmbed] })
+                : null;
+        }
+
+        const Log = new EmbedBuilder()
+            .setTitle('Message edited')
+            .setColor(colors.EMBED_INVIS_SIDEBAR)
+            .setDescription(
+                `A message by <@${oldMessage.author.id}> was edited\n\n[**Jump to message**](${newMessage.url})`
+            );
+
+        const channel = guild.channels.cache.get(`${channelLog}`);
+        return channel
+            ? channel.send({ embeds: [Log, OriginalEmbed, EditedEmbed] })
+            : null;
+    },
+};
