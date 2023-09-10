aea09c23ebe662f77576ebb65cc6f2946b7a0b9e
aName <shafaeebardia@gmail.com>
2023-09-09 21:20:58 -0700

    fixed issues in logging (#16)
    * add message deletion logging
    
    * fixed github actions issues
    
    * add message event listeners and logging
    
    * added member logging
    
    * fixed colors and rules channel display

---
+++ src/bot/events/GuildMember/guildMemberAdd.js
@@ -0,0 +1,43 @@
+const { EmbedBuilder, GuildMember, Events } = require('discord.js');
+const Settings = require('../../../private/mongodb/guildSettings.js');
+const colors = require('../../../../config/colors.json');
+
+module.exports = {
+    name: Events.GuildMemberAdd,
+    /**
+     *
+     * @param {GuildMember} member
+     */
+    async execute(member) {
+        const { user, guild } = member;
+
+        const settings = await Settings.findOne({ GuildID: guild.id });
+        if (!settings) return;
+        const channelLog = settings.MemberLogChannel;
+        const rulesChannel = settings.RulesChannel;
+        if (!channelLog || !rulesChannel) return;
+
+        const embed = new EmbedBuilder()
+            .setColor(user.hexAccentColor || 'Random')
+            .setAuthor({
+                name: user.username,
+                iconURL: user.avatarURL({ forceStatic: false, size: 512 }),
+            })
+            .setDescription(
+                `Welcome to **${
+                    guild.name
+                }**!\nPlease read the <#${rulesChannel}> before engaging in activities in this server!\n\nAccount created <t:${parseInt(
+                    user.createdTimestamp / 1000
+                )}:R>`
+            )
+            .setFooter({
+                text: `ID: ${member.id} • Member #${member.guild.memberCount}`,
+            })
+            .setTimestamp();
+
+        await guild.channels.cache
+            .get(`${channelLog}`)
+            .send({ content: `<@${member.id}>`, embeds: [embed] })
+            .catch(e => console.log(e));
+    },
+};
